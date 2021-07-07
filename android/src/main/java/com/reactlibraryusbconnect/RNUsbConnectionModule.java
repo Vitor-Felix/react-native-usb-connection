847c53aa49631ff0fa80120c5e38153f880b79aa
Vitor Felix <vytorfelix@gmail.com>
2021-07-07 09:50:22 -0300

    chore: saving files

--- android/src/main/java/com/reactlibraryusbconnect/RNUsbConnectionModule.java
+++ android/src/main/java/com/reactlibraryusbconnect/RNUsbConnectionModule.java
@@ -2,6 +2,10 @@
 package com.reactlibraryusbconnect;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Build;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -23,8 +27,12 @@ public class RNUsbConnectionModule extends ReactContextBaseJavaModule {
   }
 
   @ReactMethod
-  public void show(String text) {
-    Context context = getReactApplicationContext();
-    Toast.makeText(context, text, Toast.LENGTH_LONG).show();
+  public static boolean isConnected(Context context) {
+    Intent intent = context.registerReceiver(null, new IntentFilter("android.hardware.usb.action.USB_STATE"));
+    return intent.getExtras().getBoolean("connected");
   }
+//  public void show(String text) {
+//    Context context = getReactApplicationContext();
+//    Toast.makeText(context, text, Toast.LENGTH_LONG).show();
+//  }
 }
---
+++ example/react-native.config.js
@@ -0,0 +1,10 @@
+// react-native.config.js
+module.exports = {
+  dependencies: {
+    'react-native-usb-connection': {
+      platforms: {
+        // android: null, // disable Android platform, other platforms will still autolink if provided
+      },
+    },
+  },
+};
